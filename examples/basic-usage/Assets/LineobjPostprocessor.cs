05b192e29dc66b42937c18991572a700a0a382a0
Taylor Goolsby <taylorgoolsby@users.noreply.github.com>
2017-10-16 02:57:48 -0700

    NOT WORKING: first try. Saving just in case. Attempts to export each object with its own mesh

--- examples/basic-usage/Assets/LineobjPostprocessor.cs
+++ examples/basic-usage/Assets/LineobjPostprocessor.cs
@@ -6,15 +6,15 @@ using UnityEditor;
 
 public class LineobjPostprocessor : AssetPostprocessor {
     static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths) {
-        foreach (string str in importedAssets) {
-            if (str.EndsWith(".lineobj")) {
-                using (StreamReader sr = File.OpenText(str)) {
-                    string s = string.Empty;
-                    while ((s = sr.ReadLine()) != null) {
-                        Debug.Log(s);
-                    }
-                }
-            }
-        }
+        //foreach (string str in importedAssets) {
+        //    if (str.EndsWith(".lineobj")) {
+        //        using (StreamReader sr = File.OpenText(str)) {
+        //            string s = string.Empty;
+        //            while ((s = sr.ReadLine()) != null) {
+        //                //Debug.Log(s);
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
